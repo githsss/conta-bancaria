6ef08979686ae03eb9b2c1696acf1e664a87d05d
Ramoxxx <leogrdss@icloud.com>
2019-05-06 20:15:25 -0300

    Projeto banco

---
+++ src/ContaBancaria.java
@@ -0,0 +1,17 @@
+
+public class ContaBancaria 
+{
+    String nomedoCliente;
+    int numero;
+    double saldo;
+
+    void sacar()
+    {
+        System.out.println("");
+    }
+    void depositar()
+    {
+        System.out.println("");
+    }
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+
+public class Principal 
+{
+    public static void main( String [] args)
+        {
+            ContaBancaria accbank = new ContaBancaria ();
+       
+            Scanner scan = new Scanner(System.in);
+        }
+}
