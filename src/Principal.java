6cc55048a7d3a932a2937b96fc19c86ac4ff6c51
githsss <NBK.Bleikke.14@gmail.com>
2019-05-22 20:26:13 -0300

    sistema commit atualizado

--- src/Principal.java
+++ src/Principal.java
@@ -1,48 +1,48 @@
 
 import java.util.Scanner;
 
-
 public class Principal {
-    
-    public static void main( String [] args){
 
-            Scanner scan = new Scanner(System.in);
-            //menu//
-            int option;
-            
-            do {
-                System.out.println("1 - Depositar");
-                System.out.println("2 - Sacar");
-                System.out.println("3 - Nova Conta");
-                System.out.println("0 - Sair");
-                
-                System.out.println("Opção: ");
-                option = scan.nextInt();
-                
-                if (option ==0){
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        //menu//
+        int option;
+
+        do {
+            System.out.println("========= Selecione uma opção========");
+            System.out.println("1 - Criar conta");
+            System.out.println("2 - Encerrar");
+            System.out.println("3 - depositar");
+            System.out.println("0 - Opção");
+            option = scan.nextInt();
+            switch (option) {
+
+                case 1:
+                    ContaBancaria conta1 = new ContaBancaria();
+
+                    System.out.println("Digite seu nome: ");
+                    conta1.nome = scan.next();
+
+                    System.out.println("Digite a senha da conta: ");
+                    conta1.numero = scan.nextInt();
+
+                case 2:
                     System.exit(0);
-                }else if(option == 1){
+                    break;
+                case 3:
+                    ContaBancaria conta = new ContaBancaria();
                     System.out.println("Digite o valor a ser depositado da conta: ");
-                }
-                
-            } while(option!=0);
-            
-            System.out.println("Programa encerrado.");
-            //menu//
-            
-            ContaBancaria conta1 = new ContaBancaria ();
-            
-            System.out.println("Digite seu nome: ");
-            conta1.nome = scan.next();
-            
-            System.out.println("Digite a senha da conta: ");
-            conta1.numero = scan.nextInt();
-            
-            System.out.println("Digite o valor a ser sacado da conta: ");
-            conta1.sacar (scan.nextDouble());
-            
-            System.out.println("Digite o valor a ser depositado da conta: ");
-            conta1.depositar (scan.nextDouble());
-            
-        }
+                    conta.depositar(scan.nextDouble());
+                    break;
+                default:
+                    System.out.println("Opçaõ invalida !!");
+
+            }
+        } while (option != 0);
+
+//        System.out.println("Digite o valor a ser sacado da conta: ");
+//        conta1.sacar(scan.nextDouble());
+
     }
+}
